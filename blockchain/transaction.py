fac2e84ca9451bd8d0109b964432051ecab3f0a9
XavierDedenbach <35046513+XavierDedenbach@users.noreply.github.com>
2018-05-13 22:06:41 -0700

    updated transaction

---
+++ blockchain/transaction.py
@@ -0,0 +1,23 @@
+import time
+import sys
+
+class Transaction:
+    def __init__(self, assigned_to, assinged_from, subject, task):
+       '''
+
+       :param assigned_to:
+       :param assinged_from:
+       :param subject:
+       :param task:
+       :param size:
+       '''
+
+        self.assigned_to = assigned_to
+        self.assigned_from = assinged_from
+        self.subject = subject
+        self.task = task
+        self.size = self._get_size()
+
+    def _get_size()
+
+        return sys.getsizeof(sef.assigned_to) + sys.getsizeof(self.assigned_from) + sys.getsizeof(self.subject) + sys.getsizeof(self.task)
